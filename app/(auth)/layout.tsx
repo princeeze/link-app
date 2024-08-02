16b439d9d2acc552ed48f8edf0c2e2d103e296b3
Prince Eze <pchidireeze@gmail.com>
2024-08-02 13:26:00 +0100

    feat: create auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,22 @@
+import Image from "next/image";
+import logo from "@/public/logo.svg";
+import Link from "next/link";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
+    <main>
+      <div className="bg-grey-light flex flex-col gap-[51px] items-center w-screen h-screen px-6 justify-center">
+        <Link href="/">
+          <Image src={logo} alt="logo" />
+        </Link>
+        <div className="flex flex-col p-10 rounded-xl bg-white gap-10">
+          {children}
+        </div>
+      </div>
+    </main>
   );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Image from "next/image";
-import logo from "@/public/logo.svg";
+
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -36,65 +35,62 @@ export default function Login() {
   }
 
   return (
-    <div className="bg-grey-light flex items-center w-screen h-screen justify-center">
-      <Image src={logo} alt="logo" />
-      <div className="flex flex-col p-10 rounded-xl bg-white gap-10">
-        <div>
-          <h1 className="heading-m text-grey-dark pb-2">Login</h1>
-          <p className="body-m text-grey-default ">
-            Add your details below to get back into the app
-          </p>
-        </div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="Enter your email"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Enter your password"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormItem>
-              <FormControl>
-                <Button>Submit</Button>
-              </FormControl>
-            </FormItem>
-          </form>
-        </Form>
+    <>
+      <div>
+        <h1 className="heading-m text-grey-dark pb-2">Login</h1>
         <p className="body-m text-grey-default ">
-          Don&apos;t have an account?{" "}
-          <Link href={"/signup"} className="text-purple-default">
-            Create account
-          </Link>
+          Add your details below to get back into the app
         </p>
       </div>
-    </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormItem>
+            <FormControl>
+              <Button>Submit</Button>
+            </FormControl>
+          </FormItem>
+        </form>
+      </Form>
+      <p className="body-m text-grey-default ">
+        Don&apos;t have an account?{" "}
+        <Link href={"/signup"} className="text-purple-default">
+          Create account
+        </Link>
+      </p>
+    </>
   );
 }
