436e287da9376f2c3438bfa9116fadc7971876ef
Prince Eze <pchidireeze@gmail.com>
2024-08-17 12:51:02 +0100

    feat: add server actions for link
    add server actions that work with supabase to getlinks from links table and to update links

---
+++ app/(home)/dashboard/links/links.ts
@@ -0,0 +1,76 @@
+"use server";
+
+import { z } from "zod";
+
+import { createClient } from "@/utils/supabase/server";
+
+import { formSchema, linkSchema } from "@/lib/schema";
+
+export async function getLinks() {
+  const supabase = createClient();
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      throw new Error("User is not authenticated");
+    }
+
+    const userId = user.id;
+    const { data, error } = await supabase
+      .from("links")
+      .select("*")
+      .eq("user_id", userId);
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error: any) {
+    return { error: error.message };
+  }
+}
+
+export async function updateLinks(
+  newLinks: z.infer<typeof formSchema>["links"],
+) {
+  const supabase = createClient();
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      throw new Error("User is not authenticated");
+    }
+
+    const userId = user.id;
+
+    // Step 1: Delete all existing links for the user
+    const { error: deleteError } = await supabase
+      .from("links")
+      .delete()
+      .eq("user_id", userId);
+
+    if (deleteError) {
+      throw deleteError;
+    }
+
+    // Step 2: Insert the new links
+    const { error: insertError } = await supabase.from("links").insert(
+      newLinks.map((link: z.infer<typeof linkSchema>) => ({
+        user_id: userId,
+        platform: link.platform,
+        link: link.link,
+      })),
+    );
+
+    if (insertError) {
+      throw insertError;
+    }
+
+    return { success: true };
+  } catch (error: any) {
+    return { error: error.message };
+  }
+}
