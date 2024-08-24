b7244c1dfbec0b2b2f0ee148d99423325eb1ec2a
Prince Eze <pchidireeze@gmail.com>
2024-08-24 21:08:26 +0100

    feat(schema): create new schema and store for profile form

--- lib/store.ts
+++ lib/store.ts
@@ -5,10 +5,28 @@ import { formSchema } from "@/lib/schema";
 
 interface FormDataState {
   formData: z.infer<typeof formSchema>["links"];
+  profileData: {
+    name: string | undefined;
+    username: string | undefined;
+    email: string | undefined;
+    avatar: string | undefined;
+  };
+  setProfileData: (data: any) => void;
   setFormData: (data: any) => void;
+  avatar: string | undefined;
+  setAvatar: (link: string | undefined) => void;
 }
 
 export const useFormDataStore = create<FormDataState>((set) => ({
   formData: [],
+  profileData: {
+    name: undefined,
+    username: undefined,
+    email: undefined,
+    avatar: undefined,
+  },
+  avatar: undefined,
+  setAvatar: (link) => set({ avatar: link }),
+  setProfileData: (data) => set({ profileData: data }),
   setFormData: (data) => set({ formData: data }),
 }));
