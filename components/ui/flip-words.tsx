cf06bbc13e00cdae533ef3d96145a02718bb1949
Prince Eze <pchidireeze@gmail.com>
2024-08-24 21:18:55 +0100

    fix(eslint): fix lint issue that was causing build failure

--- components/ui/flip-words.tsx
+++ components/ui/flip-words.tsx
@@ -1,6 +1,9 @@
 "use client";
+
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
+
 import { cn } from "@/lib/utils";
 
 export const FlipWords = ({
@@ -58,8 +61,8 @@ export const FlipWords = ({
           position: "absolute",
         }}
         className={cn(
-          "z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2",
-          className
+          "text-neutral-900 dark:text-neutral-100 relative z-10 inline-block px-2 text-left",
+          className,
         )}
         key={currentWord}
       >
