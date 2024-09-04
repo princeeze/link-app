a7de5082efa5402751eee991d3f0a8d2ddd7f3db
Prince Eze <pchidireeze@gmail.com>
2024-09-04 17:06:11 +0100

    feat(auth): Add client component that can check if there is an active session

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -5,7 +5,6 @@ import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
 import { createClient } from "@/utils/supabase/server";
-
 import { loginSchema, signupSchema } from "@/lib/schema";
 
 export async function login(values: z.infer<typeof loginSchema>) {
@@ -49,5 +48,20 @@ export async function signup(values: z.infer<typeof signupSchema>) {
       return { error: error.errors.map((e) => e.message).join(", ") };
     }
     return { error: "Internal server error" };
+  }
+}
+
+export async function checkAuth() {
+  const supabase = createClient();
+  try {
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error) {
+      return { session: null, error };
+    }
+
+    return { session: data.session, error: null };
+  } catch (error) {
+    return { session: null, error };
   }
 }
