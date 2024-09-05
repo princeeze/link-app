1823500d51a68cd0ff416a70f436800850dc9015
Prince Eze <pchidireeze@gmail.com>
2024-09-05 13:49:42 +0100

    fix: Fix styling issues and implement loading logic

--- app/(home)/dashboard/links/page.tsx
+++ app/(home)/dashboard/links/page.tsx
@@ -2,19 +2,15 @@ import LinkForm from "@/components/layout/linkform";
 
 export default function Links() {
   return (
-    <div className="flex max-h-full w-full flex-col">
-      <div className="flex h-full flex-col gap-10">
-        <div>
-          <h1 className="heading-m pb-2 text-grey-dark">
-            Customize your links
-          </h1>
-          <p className="body-m text-grey-default">
-            Add/edit/remove links below and then share all your profiles with
-            the world!
-          </p>
-        </div>
-        <LinkForm />
+    <div className="flex flex-col gap-10">
+      <div>
+        <h1 className="heading-m pb-2 text-grey-dark">Customize your links</h1>
+        <p className="body-m text-grey-default">
+          Add/edit/remove links below and then share all your profiles with the
+          world!
+        </p>
       </div>
+      <LinkForm />
     </div>
   );
 }
